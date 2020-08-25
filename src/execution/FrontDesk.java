135991910d2d7a49f73a2083a20889d9d2d97e96
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-08-25 23:41:57 +0530

    add fields to store student details

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,13 +1,23 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Devansh Shukla (Devansh1352000)
+ *  Date: 25/08/20
+ *  Time: 10:50 PM
  *  File Name : FrontDesk.java
  * */
 package execution;
 
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String firstNameOfStudent = scanner.nextLine();
+        String middleNameOfStudent = scanner.nextLine();
+        String lastNameOfStudent = scanner.nextLine();
+        long universityRollNumberOfStudent = scanner.nextLong();
+        int numberOfBooksIssuedByStudent = scanner.nextInt();
+        Student studentDetails = new Student(firstNameOfStudent, middleNameOfStudent, lastNameOfStudent, universityRollNumberOfStudent, numberOfBooksIssuedByStudent);
     }
 }
